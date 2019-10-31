0bf7465b83a731da2de8931c7058845762b02be5
chengya <chengya@yonyou.com>
2019-10-31 19:43:47 +0800

    # 修改

--- src/storeone/actionCreat.js
+++ src/storeone/actionCreat.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import {CHANGE_VALUE,ADD_TEXT,DEL_TEXT,GET_LIST} from './actionTypes.js'
 export const changeInputAction = (value)=>({
     type:CHANGE_VALUE,
@@ -10,7 +11,29 @@ export const DelText = (index)=>({
     type:DEL_TEXT,
     index
 })
-export const getListData = (data)=>({
+// export const getListData = (data)=>({
+//     type:GET_LIST,
+//     data
+// })
+const getListData = (data)=>({
     type:GET_LIST,
     data
 })
+export const getListDataOne = ()=>{
+    return(dispatch)=>{
+        axios.get("http://api.izhangchu.com/",{
+            params:{
+                methodName:"CourseIndex",
+                version:4.4,
+                user_id:0,
+                page:1,
+                size:10
+            }
+        }).then((res)=>{
+            console.log(res.data.data.data)
+            const data = res.data.data.data
+            const action = getListData(data)
+            dispatch(action)
+        })
+    }
+}
